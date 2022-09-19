a3b08767fe911d7c219d2f0fc70f3fbe369ae0f6
JosephChing <joseph.ching.0@gmail.com>
2022-09-19 16:15:51 -0400

    Removed TMDB api key
     API keys must be removed to pass GitGuardian test.

---
+++ NetflixClone/Classes/Networking/NetworkConfiguration.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkConfiguration.swift
+//  NetflixClone
+//
+//  Created by Joseph Ching on 2022-09-19.
+//
+
+import Foundation
+
+
+class NetworkConfiguration {
+    static let baseURL: String = "https://api.themoviedb.org/"
+    static let apiKey: String = ""
+}
