5dfb909d1cffd6e19384c8fee220adbfee9d218c
JosephChing <32606695+JosephChing@users.noreply.github.com>
2022-09-26 23:10:46 -0400

    Style/format code (#6)
    * Commit week 7 code
    
    * Removed TMDB api key
    
     API keys must be removed to pass GitGuardian test.
    
    * Created UICollectionViewLayout using UICollectionViewDiffableDataSource for the home page.
    
    UICollectionViewLayout work UICollectionViewDiffableDataSource
    
    * Added extension for UIImageView
    
    Added function for loading images for UIImageView.
    
    * Changed PaginatedMovie to generic PaginatedList
    
    PaginatedList now takes generic types.
    
    * Added swiftlint
    
    Swiftlint has been added to buildphases
    
    * Added *.xcuserstate and *.DS_Store
    
    * Revert "Added *.xcuserstate and *.DS_Store"
    
    This reverts commit 6af201c1f89c3f5eff207a0bddd07be9bb25c51f.
    
    * Revert "Revert "Added *.xcuserstate and *.DS_Store""
    
    This reverts commit 9c06c06bf774b4e8f701b7c3dffbda8e8b19ffeb.
    
    * Format code as per swift-lint recomendations.
    
    Co-authored-by: Ahmed M. Hassan <ahmdmhasn@gmail.com>

--- NetflixClone/Classes/Scenes/Home/HomeViewController.swift
+++ NetflixClone/Classes/Scenes/Home/HomeViewController.swift
@@ -12,13 +12,8 @@ class HomeViewController: UIViewController {
     enum Defaults {
         static let moviesPerPage = 4
     }
-    
     fileprivate let sectionHeaderElementKind = "SectionHeader"
-    
     var collectionView: UICollectionView! = nil
-    
-    
-    
     // MARK: Properties
 
     private let remote = DiscoverMoviesRemote()
@@ -32,40 +27,36 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         configureHierarchy()
-        Section.allCases.forEach{ section in
+        Section.allCases.forEach { section in
             fetchNewPages(for: section)
         }
         configureDataSource()
     }
 }
 extension HomeViewController {
-    
     enum Section: String, CaseIterable {
         case featured = "Featured"
         case discover = "Discover"
         case trending = "Trending"
         case top = "Top of All Time"
-//        case actors = "Top Actors"
     }
-    
     private func configureHierarchy() {
         collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
         collectionView.delegate = self
-        collectionView.register(PosterCollectionViewCell.self, forCellWithReuseIdentifier: PosterCollectionViewCell.reuseIdentifier)
+        collectionView.register(
+            PosterCollectionViewCell.self,
+            forCellWithReuseIdentifier: PosterCollectionViewCell.reuseIdentifier
+        )
         collectionView.frame = view.bounds
         collectionView.backgroundColor = .white
         view.addSubview(collectionView)
     }
-    
-    
     private func createSectionHeaderRegistration() -> UICollectionView.SupplementaryRegistration<UICollectionViewListCell> {
         return UICollectionView.SupplementaryRegistration<UICollectionViewListCell>(
             elementKind: sectionHeaderElementKind
         ) { [weak self] supplementaryView, elementKind, indexPath in
             guard let self = self else { return }
-            // TODO: use switch/case to deal with different sections.
             let sectionID = self.dataSource.snapshot().sectionIdentifiers[indexPath.section]
             supplementaryView.configurationUpdateHandler = { supplementaryView, state in
                 guard let supplementaryCell = supplementaryView as? UICollectionViewListCell else { return }
@@ -74,26 +65,24 @@ extension HomeViewController {
                 contentConfiguration.text = sectionID.rawValue
                 contentConfiguration.textProperties.color = UIColor.black
                 contentConfiguration.textProperties.font = UIFont.boldSystemFont(ofSize: 19)
-                
                 supplementaryCell.contentConfiguration = contentConfiguration
                 supplementaryCell.backgroundConfiguration = .clear()
             }
         }
     }
-    
     private func configureDataSource() {
         dataSource = UICollectionViewDiffableDataSource
-        <Section, Movie>(collectionView: collectionView) {
+        <Section, Movie>(collectionView: collectionView){
             (collectionView: UICollectionView, indexPath: IndexPath, movie: Movie) -> UICollectionViewCell? in
-            
             let section = Section.allCases[indexPath.section]
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(PosterCollectionViewCell.reuseIdentifier)", for: indexPath) as! PosterCollectionViewCell
+            guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: "\(PosterCollectionViewCell.reuseIdentifier)",
+                for: indexPath) as?  PosterCollectionViewCell else { fatalError("Cannot Create cell") }
             let imageUrl = "https://image.tmdb.org/t/p/w500/\(movie.posterPath ?? "")"
             cell.configureImage(imageUrl: imageUrl)
             cell.applyEffects(for: section)
             return cell
         }
-        
         let headerRegistration = createSectionHeaderRegistration()
         dataSource.supplementaryViewProvider = { collectionView, elementKind, indexPath in
             return collectionView.dequeueConfiguredReusableSupplementary(using: headerRegistration, for: indexPath)
@@ -142,39 +131,51 @@ extension HomeViewController {
 //
 extension HomeViewController {
     private func createLayout() -> UICollectionViewLayout {
-        let sectionProvider = { (sectionIndex: Int,
-                                 layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+        let sectionProvider = {(sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment)-> NSCollectionLayoutSection? in
             let sectionID = self.dataSource.snapshot().sectionIdentifiers[sectionIndex]
-            
             var item: NSCollectionLayoutItem
             var group: NSCollectionLayoutGroup
             var section: NSCollectionLayoutSection
             var header: NSCollectionLayoutBoundarySupplementaryItem
-            
             switch sectionID {
             case .featured:
-                item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
-                group = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.7), heightDimension: .fractionalWidth(0.7*1.5)), subitems: [item])
+                let itemSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .fractionalHeight(1)
+                )
+                item = NSCollectionLayoutItem(layoutSize: itemSize)
+                let groupSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(0.7),
+                    heightDimension: .fractionalWidth(0.7*1.5)
+                )
+                group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
                 section = NSCollectionLayoutSection(group: group)
                 section.orthogonalScrollingBehavior = .groupPagingCentered
             case .trending, .discover, .top:
                 // Create Item
-                item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
+                item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .fractionalHeight(1)))
                 // Create Group
-                group = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .absolute(200 / 1.5), heightDimension: .absolute(200)), subitems: [item])
+                group = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: NSCollectionLayoutSize(
+                        widthDimension: .absolute(200 / 1.5),
+                        heightDimension: .absolute(200)),
+                    subitems: [item])
                 // Create Section
                 section = NSCollectionLayoutSection(group: group)
                 section.orthogonalScrollingBehavior = .continuousGroupLeadingBoundary
                 // Create Header
-                let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),heightDimension: .estimated(24))
+                let headerSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .estimated(24)
+                )
                 header = NSCollectionLayoutBoundarySupplementaryItem(
                     layoutSize: headerSize,
                     elementKind: "SectionHeader",
                     alignment: .top)
                 section.boundarySupplementaryItems = [header]
             }
-            
-
             return section
         }
         let layout = UICollectionViewCompositionalLayout(sectionProvider: sectionProvider)
@@ -182,8 +183,11 @@ extension HomeViewController {
     }
 }
 extension HomeViewController: UICollectionViewDelegate {
-
-    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        willDisplay cell: UICollectionViewCell,
+        forItemAt indexPath: IndexPath
+    ) {
         let snapshot = dataSource.snapshot()
         let section = snapshot.sectionIdentifiers[indexPath.section]
         if indexPath.row == (snapshot.numberOfItems(inSection: section)-1) && hasMoreMovies {
