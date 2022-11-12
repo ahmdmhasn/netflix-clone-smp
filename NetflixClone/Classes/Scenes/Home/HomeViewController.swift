533f43fe67cb9a39805545fc65c4ad6717f4f3cf
JosephChing <joseph.ching.0@gmail.com>
2022-11-12 13:16:42 -0500

    Update the appropriate section when appending to the UICollectionViewDiffableDataSource
    Without the appropriate section as a parameter, the UICollectionViewDiffableDataSource only updates the last section.

--- NetflixClone/Classes/Scenes/Home/HomeViewController.swift
+++ NetflixClone/Classes/Scenes/Home/HomeViewController.swift
@@ -44,7 +44,7 @@ class HomeViewController: UIViewController {
             .receive(on: DispatchQueue.main)
             .sink { [weak self] result in
                 guard let self = self else { return }
-                self.updateDataSource(movies: result.newMovies)
+                self.updateDataSource(movies: result.newMovies, to: result.section)
             }
             .store(in: &subscribers)
     }
@@ -55,6 +55,13 @@ extension HomeViewController {
         Section.allCases.forEach { section in
             viewModel.fetchNewPages(for: section, at: currentPage)
         }
+    }
+    
+    private func updateDataSource(movies: [Movie], to section: Section) {
+        var snapshot = dataSource.snapshot()
+        snapshot.appendItems(movies, toSection: section)
+        if movies.count > 0 { currentPage+=1 }
+        dataSource.apply(snapshot, animatingDifferences: true)
     }
 }
 
@@ -114,13 +121,6 @@ extension HomeViewController {
         currentSnapshot.appendSections([.featured, .trending, .discover, .top])
         currentSnapshot.appendItems([])
         dataSource.apply(currentSnapshot, animatingDifferences: false)
-    }
-    
-    private func updateDataSource(movies: [Movie]) {
-        var snapshot = dataSource.snapshot()
-        snapshot.appendItems(movies)
-        if movies.count > 0 { currentPage+=1 }
-        dataSource.apply(snapshot, animatingDifferences: true)
     }
 }
 
@@ -196,9 +196,6 @@ extension HomeViewController: UICollectionViewDelegate {
         let section = snapshot.sectionIdentifiers[indexPath.section]
         let items = snapshot.itemIdentifiers(inSection: section)
         let movie = items[indexPath.row]
-
-//        let viewController = MovieDetailsViewController(movie: movie)
-//        present(viewController, animated: true)
         
         let infoViewModel = MovieInfoViewModel(movie: movie)
         let infoView = MovieInfoView(viewModel: infoViewModel)
